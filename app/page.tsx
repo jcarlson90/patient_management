731c8b9e3c38381e1bb16156e186ecf0ca2129cf
jcarlson <jake.carlson90@gmail.com>
2025-01-17 13:12:10 -0800

    patientForm

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,5 @@
+import PatientForm from "@/components/ui/forms/PatientForm";
+import { Link } from "lucide-react";
 import Image from "next/image";
 
 export default function Home() {
@@ -11,8 +13,27 @@ export default function Home() {
         width={1000}
         alt="patient"
         className="mb-12 h-10 w-fit"/>
+
+        <PatientForm />
+        <div className="text-14-regular mt-20 flex justify-between">
+        <p className="justify-items-end text-dark-600 xl:text-left">
+        © 2024 CarePulse
+        </p>
+        <Link href="/?admin=true" className="text-green-500">
+        Admin 
+        </Link>
+        </div>
       </div>
     </section>
+
+    <Image
+    src="/assets/images/onboarding-img.png"
+    height={1000}
+    width={1000}
+    alt="patient"
+    className="side-img max-w-[50%]" />
    </div>
   );
-}
+};
+
+
